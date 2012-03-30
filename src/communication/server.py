55959103513966bdfe4168c64fb95385bee89469
Max <makz@max.localdomain>
2012-03-30 23:55:30 +0200

    JSON serialization and deserialization for State class

--- src/communication/server.py
+++ src/communication/server.py
@@ -1,1 +1,23 @@
-print "hello!"
+import json
+import ast
+
+class State:
+    def __init__(self):
+        self.agents = []
+        self.allowedMoves = []
+
+    def __init__(self, json):
+        self.from_json(json)
+
+    def setOwnPosition(self, position):
+        self.position = position
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
+
+    def from_json(self, json):
+        dictionary = ast.literal_eval(json)
+        self.agents = [ (x[0], x[1]) for x in dictionary["agents"]]
+        self.allowedMoves = [ (x[0], x[1]) for x in dictionary["allowedMoves"]]
+        self.position = (tuple(dictionary["position"]))
+
