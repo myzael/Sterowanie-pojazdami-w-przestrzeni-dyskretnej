7b4c375171ab95e6ea82d8b2a744a31787bab027
Jakub Mozgawa <jakub.mozgawa@gmail.com>
2013-03-07 16:04:37 +0100

    Added board dump

--- src/board/board.py
+++ src/board/board.py
@@ -1,11 +1,14 @@
+import cPickle
+
 from PIL import Image
 import networkx as nx
 import pylab
 from networkx.classes.function import get_node_attributes
 import matplotlib.pyplot as pyplot
-import cPickle
+
 
 ROBOT_ID = 'robotID'
+
 
 class Board(object):
     '''Represents a board that models a part of reality. 
@@ -18,6 +21,7 @@ class Board(object):
         if draw:
             pylab.ion()
             pylab.hold()
+            self.fig = pyplot.gcf()
             pylab.show()
             # clear axes
             ax = pyplot.axes([0, 0, 1, 1])
@@ -27,7 +31,7 @@ class Board(object):
     def addRobot(self, position, robotID):
         if not self._positionExists(position):
             raise StandardError('Cannot add robot. Position ' + str(position) + ' does not exist')
-        elif  self._positionOccupied(position) :
+        elif self._positionOccupied(position):
             raise StandardError('Cannot add robot! Position ' + str(position) + ' already occupied')
         elif self._containsRobot(robotID):
             raise StandardError('Cannot add robot. Robot of id ' + str(robotID) + ' is already on the board')
@@ -44,7 +48,8 @@ class Board(object):
         elif sourcePosition == targetPosition:
             pass
         elif not self._canMoveTo(targetPosition):
-            raise StandardError('Cannot move robot. Move from ' + str(sourcePosition) + ' to ' + str(targetPosition) + ' is illegal')
+            raise StandardError(
+                'Cannot move robot. Move from ' + str(sourcePosition) + ' to ' + str(targetPosition) + ' is illegal')
         else:
             self.history.append((sourcePosition, targetPosition))
             self.graph.node[targetPosition][ROBOT_ID] = self.graph.node[sourcePosition].pop(ROBOT_ID)
@@ -52,11 +57,21 @@ class Board(object):
     def getAllowedMoves(self, position):
         '''Does NOT require a robot tom be in the specified position'''
         return [position] + filter(self._canMoveTo, self.graph.neighbors(position))
+
     def draw(self):
-        nx.draw_networkx(self.graph, self.nodePositions, labels=self.getRobots(), node_size=150, node_color='w', font_color='k', animated=True)
+        nx.draw_networkx(self.graph, self.nodePositions, labels=self.getRobots(), node_size=150, node_color='w',
+                         font_color='k', animated=True)
+
     def refreshBoard(self):
         self.draw()
         pyplot.pause(1)
+
+    def dumpBoard(self, filename):
+        """
+        Needs a valid filename of graphical file (e.g. *.png or *.jpg)
+        :param filename:
+        """
+        self.fig.savefig(filename)
 
     def dumpHistory(self, filename):
         print len(self.history)
@@ -65,14 +80,18 @@ class Board(object):
     # Helper methods    
     def _canMoveTo(self, position):
         return self._positionExists(position) and not self._positionOccupied(position)
+
     def _positionExists(self, position):
         falg = position in self.graph.nodes()
         return falg
+
     def _positionOccupied(self, position):
         ret = ROBOT_ID in self.graph.node[position]
         return ret
+
     def _containsRobot(self, robotID):
         return robotID in get_node_attributes(self.graph, ROBOT_ID).values()
+
     def _read(self, filename, resolution=1):
         '''
         Reads the image from given path pointing to a B&W bmp file.
@@ -102,7 +121,7 @@ class Board(object):
         # remove unnecessary nodes
         for i in xrange(0, size[0]):
             for j in xrange(0, size[1]):
-                if(a[j, i] == 0):
+                if (a[j, i] == 0):
                     self.graph.remove_node((i, j))
 
         # create positions map
@@ -111,13 +130,12 @@ class Board(object):
             self.nodePositions[pair] = (pair[0] * 4, pair[1] * 4)
 
 
-
-
 if __name__ == "__main__":
     b1 = Board('test.bmp')
     b1.addRobot((0, 0), 1)
     b1.addRobot((0, 1), 2)
     b1.refreshBoard();
+    b1.dumpBoard("test1.png")
     b1.moveRobot((0, 1), (0, 2));
     b1.refreshBoard();
     b1.moveRobot((0, 2), (0, 3));
@@ -126,3 +144,5 @@ if __name__ == "__main__":
     b1.refreshBoard();
     b1.moveRobot((0, 4), (0, 3));
     b1.refreshBoard();
+    b1.dumpBoard("test2.png")
+
